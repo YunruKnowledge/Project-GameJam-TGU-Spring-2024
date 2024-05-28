8f8f94e5b8db73f4191df1150f354d7544009eac
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2024-05-28 23:30:15 +0200

    js for canvas background

--- export/main.js
+++ export/main.js
@@ -21,9 +21,161 @@ const createEntity = ({
       health: health,
       speed: speed,
     },
+    id: createUUID(),
   };
   console.log(entity);
   return entity;
+};
+
+const createUUID = () => {
+  var d = new Date().getTime(); //Timestamp
+  var d2 =
+    (typeof performance !== "undefined" &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0; //Time in microseconds since page-load or 0 if unsupported
+  return "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16; //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+};
+
+const createCanvas_background = (sprite_tile_size = 16) => {
+  const SPRITE_WIDTH = sprite_tile_size;
+  const SPRITE_HEIGHT = sprite_tile_size;
+  const BORDER_WIDTH = 0;
+  const SPACING_WIDTH = 0;
+
+  // converts a row and column of the spritesheet
+  // to coordinates in an image
+  function getTIleOffsetPosition(col, row) {
+    return {
+      x: BORDER_WIDTH + col * (SPACING_WIDTH + SPRITE_WIDTH),
+      y: BORDER_WIDTH + row * (SPACING_WIDTH + SPRITE_HEIGHT),
+    };
+  }
+
+  console.log(
+    `A sprite at position (3, 1) has pixel coordinates ${
+      getTIleOffsetPosition(3, 1).x
+    }, ${getTIleOffsetPosition(3, 1).y}`
+  );
+
+  const container = document.querySelector(".game_display_container");
+  const canvas = document.createElement("canvas");
+  canvas.width = container.clientWidth;
+  canvas.height = container.clientHeight;
+  canvas.style.position = `absolute`;
+  const context = canvas.getContext("2d");
+
+  const spriteTileSet = {
+    grass1: {
+      position: getTIleOffsetPosition(0, 1),
+    },
+    grass2: {
+      position: getTIleOffsetPosition(0, 2),
+    },
+    grass3: {
+      position: getTIleOffsetPosition(0, 3),
+    },
+    grass4: {
+      position: getTIleOffsetPosition(0, 4),
+    },
+    grass5: {
+      position: getTIleOffsetPosition(0, 5),
+    },
+    grass6: {
+      position: getTIleOffsetPosition(0, 6),
+    },
+    grass7: {
+      position: getTIleOffsetPosition(1, 6),
+    },
+    grass8: {
+      position: getTIleOffsetPosition(2, 6),
+    },
+    grass9: {
+      position: getTIleOffsetPosition(3, 6),
+    },
+    grass10: {
+      position: getTIleOffsetPosition(0, 7),
+    },
+    grass11: {
+      position: getTIleOffsetPosition(1, 7),
+    },
+    grass12: {
+      position: getTIleOffsetPosition(2, 7),
+    },
+    grass13: {
+      position: getTIleOffsetPosition(3, 7),
+    },
+  };
+  const image = new Image();
+  image.src = `./assets/mana_seed/sample.png`;
+
+  console.log(Object.entries(spriteTileSet)[0][1].position);
+  image.onload = function () {
+    //draw faceplate
+    for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+        const weight1 = 1 / 6;
+        const weight2 = 1 / 6 / 8;
+        // console.log(weight1,weight2);
+        const tileNumber = weightedRand({
+          0: weight1,
+          1: weight1,
+          2: weight1,
+          3: weight1,
+          4: weight1,
+          5: weight2,
+          6: weight2,
+          7: weight2,
+          8: weight2,
+          9: weight2,
+          10: weight2,
+          11: weight2,
+          12: weight2,
+        });
+
+        context.drawImage(
+          image,
+          getSpriteFromTileSet(tileNumber).x,
+          getSpriteFromTileSet(tileNumber).y,
+          SPRITE_WIDTH,
+          SPRITE_HEIGHT,
+          SPRITE_WIDTH * r,
+          SPRITE_WIDTH * c,
+          SPRITE_WIDTH,
+          SPRITE_HEIGHT
+        );
+
+        container.prepend(canvas);
+      }
+    }
+  };
+
+  // add check for number and string later, if string then search for Obj.entries name
+  const getSpriteFromTileSet = (indexID) => {
+    return Object.entries(spriteTileSet)[indexID][1].position;
+  };
+
+  const weightedRand = (spec) => {
+    var i,
+      sum = 0,
+      r = Math.random();
+    for (i in spec) {
+      sum += spec[i];
+      if (r <= sum) return i;
+    }
+  };
 };
 
 // GENERAL
@@ -55,6 +207,7 @@ const initGameEvents = () => {
       grid.appendChild(div);
     }
   }
+  createCanvas_background();
 };
 
 // SET- MODIFY
@@ -97,7 +250,7 @@ const entity_move = async (entity, position_x, position_y) => {
     // console.log(path[current_path_index]);
 
     if (current_path_index >= path.length) {
-      entity_lookForNewSafeSpot(entity)
+      entity_lookForNewSafeSpot(entity);
       clearPathingMove();
     }
 
@@ -110,7 +263,7 @@ const entity_move = async (entity, position_x, position_y) => {
 
     // check for light, to re-path
     if (current_path_index > 5 && tile.hasAttribute("isLit")) {
-      entity_lookForNewSafeSpot(entity)
+      entity_lookForNewSafeSpot(entity);
       clearPathingMove();
     } else if (tile.hasAttribute("isLit")) {
       const damage = 20 * Number(tile.getAttribute("light_value"));
@@ -148,7 +301,7 @@ const entity_move = async (entity, position_x, position_y) => {
       }
       if (settings.deleteSelf) {
         entities = entities.filter(function (obj) {
-          return obj.name !== entity.name;
+          return obj.id !== entity.id;
         });
         console.log(entities);
       }
@@ -196,10 +349,10 @@ const entity_findNewSafeSpot = async (returnPostitions) => {
   }
 };
 
-const entity_lookForNewSafeSpot = async (entity)=> {
+const entity_lookForNewSafeSpot = async (entity) => {
   const result = await entity_findNewSafeSpot(true);
   entity_move(entity, result.x, result.y);
-}
+};
 
 const entity_findPath = (entity = null, position = { x: 0, y: 0 }) => {
   let mapGraph = [];
@@ -258,10 +411,12 @@ const getRandomIntInclusive = (min, max) => {
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 };
-setTimeout(() => {
-  entities.push(createEntity({ name: "test" }));
-  entities.push(createEntity());
-  entities.push(createEntity({ name: "amander baller", health: 20 }));
-  entity_lookForNewSafeSpot(entities[0])
-  setInterval(() => {}, 400);
-}, 200);
+// setTimeout(() => {
+//   for (let index = 0; index < 5; index++) {
+//     entities.push(createEntity({ name: "amander baller", health: 200 }));
+//     entity_lookForNewSafeSpot(entities[index]);
+//   }
+//   // entity_lookForNewSafeSpot(entities[1]);
+//   // entity_lookForNewSafeSpot(entities[2]);
+//   setInterval(() => {}, 400);
+// }, 200);
